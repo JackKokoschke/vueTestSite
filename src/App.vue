04970f13d3395c33c9d935f48a1a288c03e11cfa
Jack Kokoschke <kokjac25@gmail.com>
2025-09-02 12:47:01 -0600

    Broke the project down into smaller chunks

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,35 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+const message = "Vue";
+const name = "bbbbb";
+const year = "2025";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <h1>Hello {{ message }}</h1>
   </main>
+  <footer>
+    <p>&copy; {{ name }} {{ year }}</p>
+  </footer>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+template {
+  border: blue dashed;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+main {
+  border: red dotted;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+h1 {
+  font-weight: bold;
+  text-align: center;
+  color: white;
+}
+footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  text-align: center;
 }
 </style>
